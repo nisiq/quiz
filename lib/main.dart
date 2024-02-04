52f073509b812bf026e3f0baf164187eafa76539
nisiq <nicole.siq2018@gmail.com>
2024-02-04 10:58:28 -0300

    using statefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,27 +2,29 @@ import 'package:flutter/material.dart';
 
 main() => runApp(PerguntaApp());
 
-// Tornando PerguntaApp um Widget
-class PerguntaApp extends StatelessWidget {
-
+class PerguntaAppState extends State<PerguntaApp> {
+  // Variável que mantém o índice da pergunta atual
   var perguntaSelecionada = 0;
 
-
+  // Método para manipular a resposta
   void responder() {
-    perguntaSelecionada++;
+    // Atualiza o estado para reconstruir o widget com a nova pergunta
+    setState(() {
+      perguntaSelecionada++;
+    });
     print(perguntaSelecionada);
   }
 
-
   @override
   Widget build(BuildContext context) {
+    // Lista de perguntas
     final List<String> perguntas = [
       'Qual é a sua cor favorita?',
       'Qual é o seu animal favorito?',
       'Qual é a sua comida favorita?',
     ];
 
-    //Filho widget de PerguntaApp é MaterialApp
+    // Estrutura principal da interface do usuário
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -30,19 +32,19 @@ class PerguntaApp extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
+            // Exibe a pergunta atual com base na perguntaSelecionada
             Text(perguntas[perguntaSelecionada]),
+            // Botões de resposta
             ElevatedButton(
               child: Text('Resposta 1'),
               onPressed: responder,
             ),
             ElevatedButton(
               child: Text('Resposta 2'),
-              // função que n recebe nenhum parametro ()
               onPressed: responder,
             ),
             ElevatedButton(
               child: Text('Resposta 3'),
-              //funçaõ arrow =>
               onPressed: responder,
             ),
           ],
@@ -52,5 +54,11 @@ class PerguntaApp extends StatelessWidget {
   }
 }
 
+class PerguntaApp extends StatefulWidget {
+  // Método que cria um Estado para o widget PerguntaApp
+  PerguntaAppState createState() {
+    return PerguntaAppState();
+  }
+}
 
-//Alguns componentes recebem mais que um widget.... (listas)
+// Alguns componentes recebem mais de um widget, como listas
